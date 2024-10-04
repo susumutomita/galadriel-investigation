060b6f82721722948e7987bb01c2518747ae3dc2
susumu tomita <oyster880@gmail.com>
2024-10-04 09:07:48 +0900

    fix

--- gradrial-web/src/App.tsx
+++ gradrial-web/src/App.tsx
@@ -47,20 +47,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <main>
         <div>
           <h2>Chat with Smart Contract</h2>
